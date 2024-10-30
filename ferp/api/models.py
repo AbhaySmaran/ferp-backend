b749ec1f73349a6dd0d44e8cfe231374a3aeb9a1
AbhaySmaran <dasabhaysmaran@gmail.com>
2024-10-30 20:48:04 +0530

    updated commit

--- ferp/api/models.py
+++ ferp/api/models.py
@@ -16,8 +16,8 @@ class Role(models.Model):
 class Department(models.Model):
     dept_id = models.AutoField(primary_key=True)
     dept_name = models.CharField(max_length=100)
-    HOD = models.CharField(max_length=100, blank=True, null=True)
-    dept_contact = models.CharField(max_length=15)
+    HOD = models.CharField(max_length=100, blank=True)
+    dept_contact = models.CharField(max_length=15,blank=True)
 
     def __str__(self):
         return self.dept_name
